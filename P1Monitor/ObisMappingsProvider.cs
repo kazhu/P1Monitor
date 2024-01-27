e651102e32ccc65f4c0b090c4a35a71e9aacce96
Kovacs Attila Zoltan <kaz@kaz.hu>
2024-01-27 23:47:10 +0100

    testing aot

--- P1Monitor/ObisMappingsProvider.cs
+++ P1Monitor/ObisMappingsProvider.cs
@@ -4,6 +4,7 @@ using P1Monitor.Model;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using P1Monitor.Options;
+using System.Diagnostics.CodeAnalysis;
 
 namespace P1Monitor;
 
@@ -14,18 +15,26 @@ public interface IObisMappingsProvider
 
 public class ObisMappingsProvider : IObisMappingsProvider
 {
-    private static readonly JsonSerializerOptions Options = new() { Converters = { new JsonStringEnumConverter<DsmrType>(), new JsonStringEnumConverter<DsmrUnit>() }, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+	private static readonly JsonSerializerOptions Options =
+		new()
+		{
+			Converters = { new JsonStringEnumConverter<DsmrType>(), new JsonStringEnumConverter<DsmrUnit>() },
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			TypeInfoResolver = SourceGenerationContext.Default
+		};
 	private readonly ILogger<ObisMappingsProvider> _logger;
 	private readonly ObisMappingsOptions _options;
 	private readonly Lazy<ObisMappingList> _mappings;
 
-    public ObisMappingsProvider(ILogger<ObisMappingsProvider> logger, IOptions<ObisMappingsOptions> options)
+	public ObisMappingsProvider(ILogger<ObisMappingsProvider> logger, IOptions<ObisMappingsOptions> options)
 	{
 		_logger = logger;
 		_options = options.Value;
 		_mappings = new Lazy<ObisMappingList>(CreateMapping, true);
 	}
 
+    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>")]
+    [UnconditionalSuppressMessage("AOT", "IL3050:Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.", Justification = "<Pending>")]
     private ObisMappingList CreateMapping()
 	{
 		string filePath = _options.MappingFile;
@@ -36,7 +45,7 @@ public class ObisMappingsProvider : IObisMappingsProvider
 
 		_logger.LogInformation("Loading mappings file {filePath}", filePath);
 		string json = File.ReadAllText(filePath);
-		Dictionary<string, DeviceMappingDescriptor> mappings = JsonSerializer.Deserialize<Dictionary<string, DeviceMappingDescriptor>>(json, Options)!;
+		var mappings = (JsonSerializer.Deserialize(json, typeof(Dictionary<string, DeviceMappingDescriptor>), Options) as Dictionary<string, DeviceMappingDescriptor>)!;
 		DeviceMappingDescriptor mapping = mappings[_options.DeviceName];
 		_logger.LogInformation("Using {Device} device mappings", _options.DeviceName);
 		return new ObisMappingList(mapping.Mapping.Select((x, i) => new ObisMapping(x.Key, x.Value.FieldName, x.Value.Type, x.Value.Unit, i)).ToList());
---
+++ P1Monitor/SourceGenerationContext.cs
@@ -0,0 +1,11 @@
+﻿using P1Monitor.Model;
+using System.Text.Json.Serialization;
+
+namespace P1Monitor;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(ObisMapping))]
+[JsonSerializable(typeof(Dictionary<string, DeviceMappingDescriptor>))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
