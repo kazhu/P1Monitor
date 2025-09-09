032fab4f0c397824c7d1fcccc290d2e708fd413b
Kovacs Attila Zoltan <kaz@kaz.hu>
2025-09-09 15:12:49 +0200

    fixed create_dpkg.sh

--- P1Monitor/dpkg/create_dpkg.sh
+++ P1Monitor/dpkg/create_dpkg.sh
@@ -4,13 +4,8 @@ set -e
 mkdir -p /tmp/p1monitor/usr/lib/p1monitor
 cp ../P1Monitor /tmp/p1monitor/usr/lib/p1monitor
 chmod +x /tmp/p1monitor/usr/lib/p1monitor/P1Monitor
-cp ../createdump /tmp/p1monitor/usr/lib/p1monitor
-chmod +x /tmp/p1monitor/usr/lib/p1monitor/createdump
-cp ../*.dll /tmp/p1monitor/usr/lib/p1monitor
 cp ../obismappings.json /tmp/p1monitor/usr/lib/p1monitor
-cp ../*.pdb /tmp/p1monitor/usr/lib/p1monitor
-cp ../*.so /tmp/p1monitor/usr/lib/p1monitor
-cp ../P1Monitor.*.json /tmp/p1monitor/usr/lib/p1monitor
+cp ../P1Monitor.dbg /tmp/p1monitor/usr/lib/p1monitor
 
 mkdir -p /tmp/p1monitor/etc/p1monitor
 cp ../appsettings.json /tmp/p1monitor/etc/p1monitor
@@ -21,8 +16,10 @@ cp p1monitor.service /tmp/p1monitor/lib/systemd/system
 mkdir -p /tmp/p1monitor/DEBIAN
 cp control /tmp/p1monitor/DEBIAN
 cp postinst /tmp/p1monitor/DEBIAN
-chmod +x /tmp/p1monitor/DEBIAN/postinst
+cp postrm /tmp/p1monitor/DEBIAN
 cp prerm /tmp/p1monitor/DEBIAN
+chmod +x /tmp/p1monitor/DEBIAN/postinst
+chmod +x /tmp/p1monitor/DEBIAN/postrm
 chmod +x /tmp/p1monitor/DEBIAN/prerm
 
 dpkg-deb --build /tmp/p1monitor
